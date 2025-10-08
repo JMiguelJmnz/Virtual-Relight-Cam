2e67b69d7940ab855e8c2ed66bb5ed9ead27e0f1
Miguel Jimenez <migueljmnz93@gmail.com>
2025-10-07 22:40:32 -0700

    trying to troubleshoot dlls

--- src/background.py
+++ src/background.py
@@ -1,6 +1,7 @@
+import cv2
 from rembg import remove
 
 def remove_background(frame):
-    # returns RGBA, we’ll just keep RGB for now
-    fg = remove(frame)
-    return fg[:, :, :3]
+    fg = remove(frame, model_dir="E:/Python/Virtual Relight Cam/models")  # returns RGBA
+    fg_bgr = cv2.cvtColor(fg, cv2.COLOR_RGBA2BGR)  # convert to 3 channels
+    return fg_bgr
